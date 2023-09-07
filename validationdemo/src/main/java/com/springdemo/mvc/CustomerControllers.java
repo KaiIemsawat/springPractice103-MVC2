567fb91da2e9efed553dd47560da23ea44e35172
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-09-06 23:14:31 -0400

    add a method to trim whitespace for String input field

--- validationdemo/src/main/java/com/springdemo/mvc/CustomerControllers.java
+++ validationdemo/src/main/java/com/springdemo/mvc/CustomerControllers.java
@@ -1,15 +1,26 @@
 package com.springdemo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerControllers {
+
+//    Trim whitespace if there is any (front and back)
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true); // true mean to trim down whitespace
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel) {
@@ -25,6 +36,10 @@ public class CustomerControllers {
 //                                 'customer' <-- need to match what declare in HTML and above value
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult) {
+
+        System.out.println("First name : |" + theCustomer.getFirstName() + "|");
+        System.out.println("Last name : |" + theCustomer.getLastName() + "|");
+
         if (theBindingResult.hasErrors()) {
             return "customer-form";
         }
